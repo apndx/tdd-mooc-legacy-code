9b08a82752f01a698deb4eb03749325522a7ecce
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2022-03-14 22:29:08 +0200

    Rename update methods in a more descriptive manner

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,22 +14,22 @@ class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        this.firstQualityUpdate(i);
+        this.doDailyQualityDecrease(i);
       } else {
-        this.secondQualityUpdate(i);
+        this.doDailyQualityIncrease(i);
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.sellInUpdate(i);
+        this.doDailySellInDecrease(i);
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            this.thirdQualityUpdate(i);
+            this.doubleQualityDecrease(i);
           } else {
-            this.fourthQualityUpdate(i);
+            this.zeroOutBackstagePassQuality(i);
           }
         } else {
-          this.fifthQualityUpdate(i);
+          this.increaseBrieQuality(i);
         }
       }
     }
@@ -37,17 +37,17 @@ class Shop {
     return this.items;
   }
 
-  fifthQualityUpdate(i) {
+  increaseBrieQuality(i) {
     if (this.items[i].quality < 50) {
       this.items[i].quality = this.items[i].quality + 1;
     }
   }
 
-  fourthQualityUpdate(i) {
+  zeroOutBackstagePassQuality(i) {
     this.items[i].quality = this.items[i].quality - this.items[i].quality;
   }
 
-  thirdQualityUpdate(i) {
+  doubleQualityDecrease(i) {
     if (this.items[i].quality > 0) {
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].quality = this.items[i].quality - 1;
@@ -55,29 +55,33 @@ class Shop {
     }
   }
 
-  sellInUpdate(i) {
+  doDailySellInDecrease(i) {
     this.items[i].sellIn = this.items[i].sellIn - 1;
   }
 
-  secondQualityUpdate(i) {
+  doDailyQualityIncrease(i) {
     if (this.items[i].quality < 50) {
       this.items[i].quality = this.items[i].quality + 1;
       if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].sellIn < 11) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-        if (this.items[i].sellIn < 6) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+        this.increaseQualityForBackstagePass(i);
       }
     }
   }
 
-  firstQualityUpdate(i) {
+  increaseQualityForBackstagePass(i) {
+    if (this.items[i].sellIn < 11) {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+    if (this.items[i].sellIn < 6) {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+  }
+
+  doDailyQualityDecrease(i) {
     if (this.items[i].quality > 0) {
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].quality = this.items[i].quality - 1;
